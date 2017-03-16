0571e075c4c92854a134a1c461c7079a3bdf3d18
Thomas <vanlicht@gmail.com>
2017-03-15 17:35:59 -0700

    first commit

---
+++ Assets/Scripts/SpinCube.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinCube : MonoBehaviour
+{
+    public float speed;
+    Vector3 localRotation;
+
+    // Use this for initialization
+    void Start ()
+    {
+        localRotation = this.transform.rotation.eulerAngles;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.localEulerAngles = new Vector3(Time.time * speed, Time.time * speed, localRotation.z);
+	}
+}
---
+++ Assets/Scripts/Teleportation.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleportation : MonoBehaviour
+{
+    Vector3 teleportPosition;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKey("right"))
+        {
+            Camera.main.transform.Rotate(new Vector3(0f, Time.time * 0.1f, 0f));
+        }
+        if (Input.GetKey("left"))
+        {
+            Camera.main.transform.Rotate(new Vector3(0f, -Time.time * 0.1f, 0f));
+        }
+        if (Input.GetKey("up"))
+        {
+            Camera.main.transform.Rotate(new Vector3(Time.time * -0.1f, 0f, 0f));
+        }
+        if (Input.GetKey("down"))
+        {
+            Camera.main.transform.Rotate(new Vector3(Time.time * 0.1f, 0f, 0f));
+        }
+        if (Input.GetKeyDown("space"))
+        {
+            Debug.Log("Spacebar");
+            RaycastHit hit;
+            if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit))
+            {
+                Debug.Log("Contact");
+                teleportPosition = hit.transform.position;
+                Camera.main.transform.position = teleportPosition;
+            }
+        }
+	}
+}
